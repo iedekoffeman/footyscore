807c602de944cb5941d4452f210abd90148b77df
iedekoffeman <i.koffeman@gmail.com>
2021-06-28 21:07:00 +0200

    Display username on profile page

--- src/pages/MyProfile.js
+++ src/pages/MyProfile.js
@@ -1,11 +1,25 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { authContext} from "../contexts/AuthContext";
 
 function MyProfile() {
 
+    const authInfo = useContext(authContext);
+    console.log("UserInfo", authInfo);
 
     return (
         <>
             <h2>My Profile</h2>
+
+            <p>
+                <strong>username:</strong>{" "}
+                {authInfo.authState.user && authInfo.authState.user.username}
+
+            </p>
+            <p>
+                <strong>E-mail:</strong>{" "}
+                {authInfo.authState.user && authInfo.authState.user.email}
+
+            </p>
 
         </>
     );
